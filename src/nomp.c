72ab6742f638a1596c52a8575d44df53b013d15f
git-rep <git-rep@openmailbox.org>
2016-09-12 18:48:59 -0300

    Add disable fields on login.

--- src/nomp.c
+++ src/nomp.c
@@ -1,13 +1,14 @@
 #include "nomp.h"
 
-#define ENTER 10
-#define ESCAPE 27
-#define QUIT 113
+#define KEY_RETURN 10
+#define KEY_ESCAPE 27
+#define KEY_QUIT 113
 
 FIELD *field[9];
 FORM *form;
 WINDOW *window; 
 
+int i;
 int n_fields = ((sizeof(field) / sizeof(field[0])) - 2);
 
 void init()
@@ -21,11 +22,11 @@ void init()
     if (has_colors()) {
         start_color();
         init_color(COLOR_GREEN, 0, 200, 0);
-        init_color(COLOR_RED, 150, 0, 0);
+        init_color(COLOR_BLUE, 150, 150, 150);
         init_pair(1, COLOR_BLACK, COLOR_WHITE);
         init_pair(2, COLOR_WHITE, COLOR_BLACK);
         init_pair(3, COLOR_WHITE, COLOR_GREEN);
-        init_pair(4, COLOR_WHITE, COLOR_RED);
+        init_pair(4, COLOR_WHITE, COLOR_BLUE);
     }
 
     ui();
@@ -35,7 +36,6 @@ void ui()
 { 
     int rows;
     int cols;
-    int i;
     
     field[0] = new_field(1, 30, 0, 1, 0, 0);
     field[1] = new_field(1, 30, 3, 1, 0, 0);
@@ -49,13 +49,13 @@ void ui()
     
     set_field_back(field[0], COLOR_PAIR(3));
     
-    field_opts_off(field[3], O_PUBLIC);
-    field_opts_off(field[4], O_ACTIVE);
-    
     for (i = 0; i <= n_fields; i++) {
+        set_field_just(field[i], JUSTIFY_CENTER);
         field_opts_off(field[i], O_AUTOSKIP);
-        if (i >= 4)
+        if (i >= 4) {
+            field_opts_off(field[i], O_ACTIVE);
             set_field_back(field[i], COLOR_PAIR(4));
+        }
     }
     
     set_field_buffer(field[0], 0, "localhost");
@@ -72,7 +72,7 @@ void ui()
 
     post_form(form);
     
-    wattron(window, A_BOLD | COLOR_PAIR(1));
+    wattron(window,  A_ITALIC | COLOR_PAIR(1));
     mvwprintw(window, 1, 3, "HOST");
     mvwprintw(window, 4, 3, "PORT");
     mvwprintw(window, 7, 3, "USER");
@@ -81,7 +81,7 @@ void ui()
     mvwprintw(window, 18, 3, "SCAN");
     mvwprintw(window, 23, 3, "TASK");
     mvwprintw(window, 26, 3, "UPDATE");
-    wattroff(window, A_BOLD | COLOR_PAIR(1));
+    wattroff(window, A_ITALIC | COLOR_PAIR(1));
 
     wrefresh(stdscr);
     wrefresh(window);
@@ -106,6 +106,7 @@ void driver()
 { 
     int key;
     int c_field = 0;
+    int a_fields = 3;
 
     do {
         key = wgetch(window);
@@ -113,37 +114,43 @@ void driver()
 		{	
             case KEY_DOWN:
                 set_field_back(field[c_field], COLOR_PAIR(2));
-                if (c_field == n_fields)
+                if (c_field == a_fields)
                     c_field = 0;
                 else
                     ++c_field;
-                set_field_back(field[c_field], COLOR_PAIR(3));
                 form_driver(form, REQ_NEXT_FIELD);
                 form_driver(form, REQ_END_LINE);
+                set_field_back(field[c_field], COLOR_PAIR(3));
                 break;
             case KEY_UP:
                 set_field_back(field[c_field], COLOR_PAIR(2));
                 if (c_field == 0)
-                    c_field = n_fields;
+                    c_field = a_fields;
                 else
                     --c_field;
-                set_field_back(field[c_field], COLOR_PAIR(3));
                 form_driver(form, REQ_PREV_FIELD);
                 form_driver(form, REQ_END_LINE);
+                set_field_back(field[c_field], COLOR_PAIR(3));
+                break;
+            case KEY_RETURN:
+                a_fields = n_fields;
+                for (i = 0; i <= a_fields; i++) {
+                    set_field_back(field[i], COLOR_PAIR(2));
+                    field_opts_on(field[i], O_ACTIVE);
+                }
+                //field_opts_off(field[i], O_AUTOSKIP | O_EDIT);
                 break;
             default:
                 form_driver(form, key);
                 break;
         }
-    } while (key != QUIT);
+    } while (key != KEY_QUIT);
     
     quit();
 }
 
 void quit()
 {
-    int i;
-
     unpost_form(form);
     
     free_form(form);
