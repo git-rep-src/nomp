5784d26b98c9858631a3328deb70891f0832c138
git-rep <git-rep@openmailbox.org>
2016-09-12 15:08:23 -0300

    Unify UI.

--- src/nomp.c
+++ src/nomp.c
@@ -4,13 +4,11 @@
 #define ESCAPE 27
 #define QUIT 113
 
-FIELD *field_login[5];
-FORM *form_login;
-WINDOW *win_login; 
+FIELD *field[9];
+FORM *form;
+WINDOW *window; 
 
-FIELD *field_target[5];
-FORM *form_target;
-WINDOW *win_target; 
+int n_fields = ((sizeof(field) / sizeof(field[0])) - 2);
 
 void init()
 {
@@ -22,100 +20,71 @@ void init()
 
     if (has_colors()) {
         start_color();
-        init_color(COLOR_BLUE, 170, 170, 170);
+        init_color(COLOR_GREEN, 0, 200, 0);
+        init_color(COLOR_RED, 150, 0, 0);
         init_pair(1, COLOR_BLACK, COLOR_WHITE);
         init_pair(2, COLOR_WHITE, COLOR_BLACK);
-        init_pair(3, COLOR_WHITE, COLOR_BLUE);
+        init_pair(3, COLOR_WHITE, COLOR_GREEN);
+        init_pair(4, COLOR_WHITE, COLOR_RED);
     }
 
-    //login();
-    target();
+    ui();
 }
 
-void login()
+void ui()
 { 
     int rows;
     int cols;
+    int i;
     
-    field_login[0] = new_field(1, 30, 0, 1, 0, 0);
-    field_login[1] = new_field(1, 30, 3, 1, 0, 0);
-    field_login[2] = new_field(1, 30, 6, 1, 0, 0);
-    field_login[3] = new_field(1, 30, 9, 1, 0, 0);
-    field_login[4] = NULL;
+    field[0] = new_field(1, 30, 0, 1, 0, 0);
+    field[1] = new_field(1, 30, 3, 1, 0, 0);
+    field[2] = new_field(1, 30, 6, 1, 0, 0);
+    field[3] = new_field(1, 30, 9, 1, 0, 0);
+    field[4] = new_field(1, 30, 14, 1, 0, 0);
+    field[5] = new_field(1, 30, 17, 1, 0, 0);
+    field[6] = new_field(1, 30, 22, 1, 0, 0);
+    field[7] = new_field(1, 30, 25, 1, 0, 0);
+    field[8] = NULL;
     
-    set_field_buffer(field_login[0], 0, "localhost");
-    set_field_buffer(field_login[1], 0, "9390");
+    set_field_back(field[0], COLOR_PAIR(3));
     
-    field_opts_off(field_login[0], O_AUTOSKIP);
-    field_opts_off(field_login[1], O_AUTOSKIP);
-    field_opts_off(field_login[2], O_AUTOSKIP);
-    field_opts_off(field_login[3], O_AUTOSKIP | O_PUBLIC);
-	
-    form_login = new_form(field_login);
-    scale_form(form_login, &rows, &cols);
+    field_opts_off(field[3], O_PUBLIC);
+    field_opts_off(field[4], O_ACTIVE);
     
-    win_login = create_window(rows + 6, cols + 4, ((LINES - 14) / 2), ((COLS - 36) / 2));
-    keypad(win_login, TRUE);
+    for (i = 0; i <= n_fields; i++) {
+        field_opts_off(field[i], O_AUTOSKIP);
+        if (i >= 4)
+            set_field_back(field[i], COLOR_PAIR(4));
+    }
     
-    set_form_win(form_login, win_login);
-    set_form_sub(form_login, derwin(win_login, rows, cols, 2, 2));
+    set_field_buffer(field[0], 0, "localhost");
+    set_field_buffer(field[1], 0, "9390");
+    
+    form = new_form(field);
+    scale_form(form, &rows, &cols);
 
-    post_form(form_login);
+    window = create_window(rows + 6, cols + 4, 1, ((COLS - 36) / 2));
+    keypad(window, TRUE);
     
-    wattron(win_login, COLOR_PAIR(1));
-    mvwprintw(win_login, 1, 3, "HOST");
-    mvwprintw(win_login, 4, 3, "PORT");
-    mvwprintw(win_login, 7, 3, "USER");
-    mvwprintw(win_login, 10, 3, "PASSWORD");
-    wattroff(win_login, COLOR_PAIR(1));
+    set_form_win(form, window);
+    set_form_sub(form, derwin(window, rows, cols, 2, 2));
 
-    wrefresh(stdscr);
-    wrefresh(win_login);
-
-    driver();
-}
-
-void target()
-{ 
-    int rows;
-    int cols;
+    post_form(form);
     
-    field_target[0] = new_field(1, 30, 0, 1, 0, 0);
-    field_target[1] = new_field(1, 30, 3, 1, 0, 0);
-    field_target[2] = new_field(1, 30, 6, 1, 0, 0);
-    field_target[3] = new_field(1, 30, 9, 1, 0, 0);
-    field_target[4] = NULL;
-    
-    set_field_back(field_target[0], COLOR_PAIR(3));
-    
-    set_field_buffer(field_target[0], 0, "localhost");
-    set_field_buffer(field_target[1], 0, "9390");
-    
-    field_opts_off(field_target[0], O_AUTOSKIP);
-    field_opts_off(field_target[1], O_AUTOSKIP);
-    field_opts_off(field_target[2], O_AUTOSKIP);
-    field_opts_off(field_target[3], O_AUTOSKIP);// | O_PUBLIC);
-    
-    form_target = new_form(field_target);
-    scale_form(form_target, &rows, &cols);
-
-    win_target = create_window(rows + 6, cols + 4, ((LINES - 14) / 2), ((COLS - 36) / 2));
-    keypad(win_target, TRUE);
-    
-    set_form_win(form_target, win_target);
-    set_form_sub(form_target, derwin(win_target, rows, cols, 2, 2));
-
-    post_form(form_target);
-    
-    wattron(win_target, COLOR_PAIR(1));
-    mvwprintw(win_target, 1, 3, "HOST");
-    mvwprintw(win_target, 4, 3, "PORT");
-    mvwprintw(win_target, 7, 3, "USER");
-    mvwprintw(win_target, 10, 3, "PASSWORD");
-    wattroff(win_target, COLOR_PAIR(1));
+    wattron(window, A_BOLD | COLOR_PAIR(1));
+    mvwprintw(window, 1, 3, "HOST");
+    mvwprintw(window, 4, 3, "PORT");
+    mvwprintw(window, 7, 3, "USER");
+    mvwprintw(window, 10, 3, "PASSWORD");
+    mvwprintw(window, 15, 3, "TARGET");
+    mvwprintw(window, 18, 3, "SCAN");
+    mvwprintw(window, 23, 3, "TASK");
+    mvwprintw(window, 26, 3, "UPDATE");
+    wattroff(window, A_BOLD | COLOR_PAIR(1));
 
     wrefresh(stdscr);
-    wrefresh(win_target);
+    wrefresh(window);
 
     driver();
 }
@@ -137,69 +106,51 @@ void driver()
 { 
     int key;
     int c_field = 0;
-    int n_fields = ((sizeof(field_target) / sizeof(field_target[0])) - 2);
 
     do {
-        key = wgetch(win_target);
+        key = wgetch(window);
         switch(key)
 		{	
             case KEY_DOWN:
-                set_field_back(field_target[c_field], COLOR_PAIR(2));
+                set_field_back(field[c_field], COLOR_PAIR(2));
                 if (c_field == n_fields)
                     c_field = 0;
                 else
                     ++c_field;
-                set_field_back(field_target[c_field], COLOR_PAIR(3));
-                form_driver(form_target, REQ_NEXT_FIELD);
-                form_driver(form_target, REQ_END_LINE);
+                set_field_back(field[c_field], COLOR_PAIR(3));
+                form_driver(form, REQ_NEXT_FIELD);
+                form_driver(form, REQ_END_LINE);
                 break;
             case KEY_UP:
-                set_field_back(field_target[c_field], COLOR_PAIR(2));
+                set_field_back(field[c_field], COLOR_PAIR(2));
                 if (c_field == 0)
-                    c_field = 3;
+                    c_field = n_fields;
                 else
                     --c_field;
-                set_field_back(field_target[c_field], COLOR_PAIR(3));
-                form_driver(form_target, REQ_PREV_FIELD);
-                form_driver(form_target, REQ_END_LINE);
+                set_field_back(field[c_field], COLOR_PAIR(3));
+                form_driver(form, REQ_PREV_FIELD);
+                form_driver(form, REQ_END_LINE);
                 break;
             default:
-                form_driver(form_target, key);
+                form_driver(form, key);
                 break;
         }
     } while (key != QUIT);
-/*    
-    do {
-        key = wgetch(win_login);
-        switch(key)
-		{	
-            case KEY_DOWN:
-                form_driver(form_login, REQ_NEXT_FIELD);
-                form_driver(form_login, REQ_END_LINE);
-                break;
-            case KEY_UP:
-                form_driver(form_login, REQ_PREV_FIELD);
-                form_driver(form_login, REQ_END_LINE);
-                break;
-            default:
-                form_driver(form_login, key);
-                break;
-        }
-    } while (key != QUIT);
-*/
-    cleanup();
+    
+    quit();
 }
 
-void cleanup()
+void quit()
 {
-    unpost_form(form_login);
+    int i;
+
+    unpost_form(form);
     
-    free_form(form_login);
-	free_field(field_login[0]);
-	free_field(field_login[1]); 
-	free_field(field_login[2]); 
-	free_field(field_login[3]); 
+    free_form(form);
     
-    delwin(win_login);
+    for (i = 0; i <= n_fields; i++)
+	    free_field(field[i]);
+    
+    delwin(window);
     endwin();
 }
--- src/nomp.h
+++ src/nomp.h
@@ -1,9 +1,8 @@
 #include <form.h>
 
 void init();
-void login();
-void target();
+void ui();
 WINDOW *create_window(int height, int width, int starty, int startx);
 void cmd();
 void driver();
-void cleanup();
+void quit();
