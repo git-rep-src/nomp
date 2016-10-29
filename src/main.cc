20bdb69bcf793ca3101e7115f5ea6b460efae58e
git-rep <git-rep@openmailbox.org>
2016-10-29 16:01:15 -0300

    Add info message.

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,18 @@
 #include "nomp.h"
 
+#include <iostream>
+
 int main (int argc, char *argv[])
 {	
+    if (argc > 1) {
+        std::cout << std::endl;
+        std::cout << "A simple Ncurses OpenVAS client (https://github.com/git-rep/nomp)" << std::endl;
+        std::cout << "Version 0.1" << std::endl;
+        std::cout << std::endl;
+        return 0;
+    }
+    
     Nomp nomp;
+    
     return 0;
 }
